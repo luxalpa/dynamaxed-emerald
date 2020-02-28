8f0503116090a5c475b4844213dc12d5694b10e6
Tyranteon <git@tyranteon.com>
2020-02-28 20:10:57 +0100

    First step for item serialization

--- src/item.c
+++ src/item.c
@@ -28,8 +28,7 @@ static bool8 CheckPyramidBagHasSpace(u16 itemId, u16 count);
 EWRAM_DATA struct BagPocket gBagPockets[POCKETS_COUNT] = {0};
 
 // rodata
-#include "data/text/item_descriptions.h"
-#include "data/items.h"
+#include "data/generated/items.h"
 
 // code
 static u16 GetBagItemQuantity(u16 *quantity)
